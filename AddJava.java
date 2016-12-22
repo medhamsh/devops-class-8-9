74b7ef7aaaec86ad14530f7bdc007fbb0436e725
LaxmanaRao <laxmanaraogaridepalli@gmail.com>
2016-12-22 00:03:30 -0800

    Adding Simple Java Code

---
+++ AddJava.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class AddSum{
+public static void main(String args[])
+{
+int x,y,z;
+System.out.println(Enter two numbers :);
+Scanner in = new Scanner(System.in);
+x = in.nextInt();
+y = in.nextInt();
+z = x+y;
+System.out.println(Sum of Two Numbers is :  +z);
+}
+}
